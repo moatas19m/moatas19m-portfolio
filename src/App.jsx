6659ac022131c18965a0654ef1cc7169abdcb46a
moatas19m <sayyid.moatasim2@gmail.com>
2025-10-03 06:09:39 +0500

    feat: Added routes and pages, installed react router dom, updated react packages for type safety

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import './index.css';
-import HeroScene from './components/HeroScene.jsx';
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HeroScene from './components/HeroScene.jsx'
+import ProjectsPage from './pages/ProjectsPage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import WorkPage from './pages/WorkPage.jsx'
+import SkillsPage from './pages/SkillsPage.jsx'
+import HobbiesPage from './pages/HobbiesPage.jsx'
+import ContactPage from './pages/ContactPage.jsx'
+import React from 'react';
 
 export default function App() {
     return (
-        <div className="relative min-h-screen bg-black text-white">
-            {/* 3D background */}
-            <HeroScene />
+        <BrowserRouter>
+            <div className="relative min-h-screen bg-black text-white">
+                <Routes>
+                    <Route path="/" element={<HeroScene />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/hobbies" element={<HobbiesPage />} />
+                    <Route path="/projects" element={<ProjectsPage />} />
+                    <Route path="/work" element={<WorkPage />} />
+                    <Route path="/skills" element={<SkillsPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
 
-            {/* Fade at bottom for cinematic feel */}
-            <div className="pointer-events-none absolute inset-x-0 bottom-0 h-24 bg-gradient-to-t from-black to-transparent z-10" />
-        </div>
-    );
+                {/* Fade at bottom for cinematic feel */}
+                <div className="pointer-events-none absolute inset-x-0 bottom-0 h-24 bg-gradient-to-t from-black to-transparent z-10" />
+            </div>
+        </BrowserRouter>
+    )
 }
